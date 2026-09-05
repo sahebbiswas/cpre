585a14e1bdb0b2a743b6de0dd166f71a67e7a5d6
Saheb <sahebbiswas@gmail.com>
2026-09-04 18:19:13 -0600

    Update version test for macro assumptions API

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,5 @@
 import cpre
 
 
-def test_version_for_structured_edit_api():
-    assert cpre.__version__ == "0.5.0"
+def test_version_for_macro_assumptions_api():
+    assert cpre.__version__ == "0.6.0"
